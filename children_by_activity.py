eda79c7c37ad37e14662b7e8572b3e8f17ed4878
Jennyfer Carneiro <jennyfer.feliciano97@gmail.com>
2025-06-10 11:52:21 -0300

    Replace for loop with set for improved performance

--- children_by_activity.py
+++ children_by_activity.py
@@ -1,7 +1,7 @@
 """ Report of children by activity / Relatório de crianças por atividade
 
 """
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 sala1 = ["Erik", "Maria", "Gustavo", "Manuel", "Sofia", "Joana"]
 sala2 = ["Joao", "Antonio", "Carlos", "Maria", "Isolda"]
@@ -16,14 +16,8 @@ atividades = [
     ("Dança", aula_danca)]
 
 for nome_atividade, atividade in atividades:
-    atividade_sala1 = []
-    atividade_sala2 = []
-
-    for aluno in atividade:
-        if aluno in sala1:
-            atividade_sala1.append(aluno)
-        elif aluno in sala2:
-            atividade_sala2.append(aluno)
+    atividade_sala1 = set(sala1) & set(atividade)
+    atividade_sala2 = set(sala2).intersection(atividade)
 
     print(f"{nome_atividade} sala 1", atividade_sala1)
     print(f"{nome_atividade} sala 2", atividade_sala2)
